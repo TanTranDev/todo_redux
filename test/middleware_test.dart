c916081a25b737fc481c8fb4c3c9f6d982b2baa4
TanTranDev <nhattan.tran@manabie.com>
2021-12-06 15:56:10 +0700

    [06/12/21] complete middleware test

--- test/middleware_test.dart
+++ test/middleware_test.dart
@@ -14,65 +14,18 @@ import 'package:task_management_redux/store/reducers/reducer.dart';
 import 'middleware_test.mocks.dart';
 
 @GenerateMocks([AbstractRepository])
-// void main() {
-//   group("Test Middleware", () {
-//     test("Test get all Tasks", () async {
-//       final mockAbstractRepository = MockAbstractRepository();
-//       when(mockAbstractRepository.getAllTasks())
-//           .thenAnswer((realInvocation) async => BuiltList<Task>([
-//                 Task((b) => b..title = "Title"),
-//               ]));
-//       expect(
-//           await mockAbstractRepository.getAllTasks(),
-//           BuiltList<Task>([
-//             Task((b) => b..title = "Title"),
-//           ]));
-//     });
-//     test("Test create Task", () async {
-//       final mockAbstractRepository = MockAbstractRepository();
-//       when(mockAbstractRepository
-//               .createTask(Task((b) => b..title = "New Task")))
-//           .thenAnswer((realInvocation) async => 0);
-//       expect(
-//           await mockAbstractRepository
-//               .createTask(Task((b) => b..title = "New Task")),
-//           0);
-//     });
-//     test("Test update Task", () async {
-//       final mockAbstractRepository = MockAbstractRepository();
-//       when(mockAbstractRepository
-//               .updateTask(Task((b) => b..title = "New Task")))
-//           .thenAnswer((realInvocation) async => 0);
-//       expect(
-//           await mockAbstractRepository
-//               .updateTask(Task((b) => b..title = "New Task")),
-//           0);
-//     });
-//     test("Test delete Task", () async {
-//       final mockAbstractRepository = MockAbstractRepository();
-//       when(mockAbstractRepository
-//               .deleteTask(Task((b) => b..title = "New Task")))
-//           .thenAnswer((realInvocation) async => 0);
-//       expect(
-//           await mockAbstractRepository
-//               .deleteTask(Task((b) => b..title = "New Task")),
-//           0);
-//     });
-//   });
-// }
-
 void main() {
-  group("Test middleware", () {
-    MockAbstractRepository mockAbstractRepository = MockAbstractRepository();
-    AppState firstState = AppState();
-    AppMiddleware appMiddleware = AppMiddleware(mockAbstractRepository);
-    final store = Store<AppState>(appStateReducer,
-        initialState: firstState,
-        middleware: [EpicMiddleware<AppState>(appMiddleware)]);
-
+  MockAbstractRepository mockAbstractRepository = MockAbstractRepository();
+  AppState firstState = AppState();
+  AppMiddleware appMiddleware = AppMiddleware(mockAbstractRepository);
+  final store = Store<AppState>(appStateReducer,
+      initialState: firstState,
+      middleware: [EpicMiddleware<AppState>(appMiddleware)]);
+  Task task = Task((b) => b..title = "Title");
+  group("Test middleware get all tasks", () {
     // set up run test
     runTest(
-        {required Future<dynamic> Function(Invocation) mockAnswer,
+        {required Future<BuiltList<Task>> Function(Invocation) mockAnswer,
         required action,
         required dynamic matcher}) async {
       Stream<dynamic> stream = appMiddleware.call(
@@ -81,27 +34,124 @@ void main() {
           store,
         ),
       );
+      when(mockAbstractRepository.getAllTasks()).thenAnswer(mockAnswer);
       expect(
-        await stream.toList(),
-        matcher,
+        await stream.toList().then((value) => value.toString()),
+        matcher.toString(),
       );
     }
     // end set up run test
 
-    test("Test", () async {
+    test("let test get all tasks", () async {
       Task task = Task((b) => b..title = "Title");
       List<Task> tasks = [
         task,
       ];
       await runTest(
-        mockAnswer: (_) async {},
-        action: CreateTaskMiddlewareTaskAction.create(newTask: "Title"),
+        mockAnswer: (_) async {
+          return BuiltList<Task>([
+            task,
+          ]);
+        },
+        action: GetAllTaskMiddlewareTaskAction(),
         matcher: [
           ChangeStatusReducerAppAction.create(newStatus: "isLoading"),
-          ChangeTasksReducerAppAction.create(newTasks: tasks),
+          ChangeTasksReducerAppAction.create(newTasks: tasks.toList()),
           ChangeStatusReducerAppAction.create(newStatus: "idle"),
         ],
       );
+    });
+
+    group("Test Middleware create, update, delete a task", () {
+      // set up run test
+      runTest(
+          {required Future<BuiltList<Task>> Function(Invocation) mockAnswer,
+          required Future<int> Function(Invocation) mockAnswer2,
+          required action,
+          required dynamic matcher}) async {
+        Stream<dynamic> stream = appMiddleware.call(
+          Stream.fromIterable([action]).asBroadcastStream(),
+          EpicStore<AppState>(
+            store,
+          ),
+        );
+        when(mockAbstractRepository.createTask(task)).thenAnswer(mockAnswer2);
+        when(mockAbstractRepository.updateTask(task)).thenAnswer(mockAnswer2);
+        when(mockAbstractRepository.deleteTask(task)).thenAnswer(mockAnswer2);
+        when(mockAbstractRepository.getAllTasks()).thenAnswer(mockAnswer);
+        expect(
+          await stream.toList().then((value) => value.toString()),
+          matcher.toString(),
+        );
+      }
+
+      // end set up run test
+      test("let test create a new task", () async {
+        Task task = Task((b) => b..title = "Title");
+        List<Task> tasks = [
+          task,
+        ];
+        await runTest(
+          mockAnswer: (_) async {
+            return BuiltList<Task>([
+              task,
+            ]);
+          },
+          mockAnswer2: (_) async {
+            return 0;
+          },
+          action: CreateTaskMiddlewareTaskAction.create(newTask: "Title"),
+          matcher: [
+            ChangeStatusReducerAppAction.create(newStatus: "isLoading"),
+            ChangeTasksReducerAppAction.create(newTasks: tasks.toList()),
+            ChangeStatusReducerAppAction.create(newStatus: "idle"),
+          ],
+        );
+      });
+      test("let test update a new task", () async {
+        Task task = Task((b) => b..title = "Title");
+        List<Task> tasks = [
+          task,
+        ];
+        await runTest(
+          mockAnswer: (_) async {
+            return BuiltList<Task>([
+              task,
+            ]);
+          },
+          mockAnswer2: (_) async {
+            return 0;
+          },
+          action: UpdateTaskMiddlewareTaskAction.create(updateTask: task),
+          matcher: [
+            ChangeStatusReducerAppAction.create(newStatus: "isLoading"),
+            ChangeTasksReducerAppAction.create(newTasks: tasks.toList()),
+            ChangeStatusReducerAppAction.create(newStatus: "idle"),
+          ],
+        );
+      });
+      test("let test delete a new task", () async {
+        Task task = Task((b) => b..title = "Title");
+        List<Task> tasks = [
+          task,
+        ];
+        await runTest(
+          mockAnswer: (_) async {
+            return BuiltList<Task>([
+              task,
+            ]);
+          },
+          mockAnswer2: (_) async {
+            return 0;
+          },
+          action: DeleteTaskMiddlewareTaskAction.create(deleteTask: task),
+          matcher: [
+            ChangeStatusReducerAppAction.create(newStatus: "isLoading"),
+            ChangeTasksReducerAppAction.create(newTasks: tasks.toList()),
+            ChangeStatusReducerAppAction.create(newStatus: "idle"),
+          ],
+        );
+      });
     });
   });
 }
